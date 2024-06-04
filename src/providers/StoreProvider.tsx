021df90eeeeec40fb776f507d9dca892f2a2f7d3
mujibur-rohman <muji.official06@gmail.com>
2024-06-04 09:31:07 +0700

    persist cart

--- src/providers/StoreProvider.tsx
+++ src/providers/StoreProvider.tsx
@@ -3,13 +3,21 @@
 import { store } from "@/store";
 import React from "react";
 import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 type Props = {
   children: React.ReactNode;
 };
 
+const persistor = persistStore(store);
+
 function StoreProvider({ children }: Props) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>{children}</PersistGate>
+    </Provider>
+  );
 }
 
 export default StoreProvider;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cart";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  cart: cartReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
